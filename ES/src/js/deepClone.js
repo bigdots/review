a5095c728e0c8463a8043333cb9746a1ffa3d8ce
Sean <18058799212@163.com>
2021-09-09 17:04:49 +0800

    vue

--- ES/src/js/deepClone.js
+++ ES/src/js/deepClone.js
@@ -1,13 +1,13 @@
 
 
-function noClone(value){
+function noClone(value) {
     const type = typeof value;
     return type !== "object" || type == null
 }
 
 
-function deepClone(tar){
-    if(noClone(tar)){
+function deepClone(tar) {
+    if (noClone(tar)) {
         // 如果是基本类型值，直接返回
         return tar
     }
@@ -15,19 +15,19 @@ function deepClone(tar){
 
     // 初始化结果
     let result;
-    if(tar instanceof Array){
-        result =[];
-    }else{
-        result ={}
+    if (tar instanceof Array) {
+        result = [];
+    } else {
+        result = {}
     }
 
     for (const key in tar) {
         // 判断是否为原型链上的属性
         if (Object.hasOwnProperty.call(tar, key)) {
             const element = tar[key];
-            if(noClone(element)){
+            if (noClone(element)) {
                 result[key] = element
-            }else{
+            } else {
                 //递归
                 result[key] = deepClone(element)
             }
@@ -53,5 +53,4 @@ let a = {
 let b = deepClone(a);
 a.address.city = "beijing";
 console.log(b.address.city)  // beijing
-
 
